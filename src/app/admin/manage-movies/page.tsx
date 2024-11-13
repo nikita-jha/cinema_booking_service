6ce6a0200e0372629685629f1f6944b23aa49e4f
Nikita Jha <nikitajha1912@gmail.com>
2024-11-12 22:42:26 -0500

    Fixing schedule method

--- src/app/admin/manage-movies/page.tsx
+++ src/app/admin/manage-movies/page.tsx
@@ -104,7 +104,7 @@ const AdminPortalHomePage = () => {
                       <EditMovie movie={movie} onMovieUpdated={fetchMovies} />
                       <ScheduleMovie
                         movie={movie}
-                        onMovieUpdated={fetchMovies}
+                        onScheduleAdded={fetchMovies}
                       />
                       <button
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
